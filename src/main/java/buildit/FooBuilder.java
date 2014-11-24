79885942d1d377bc17e3699894c9015b3d9a70b8
Alexey Gavrilov <agavrilov@telenordigital.com>
2014-11-24 23:12:05 +0100

    Adds equals and hashCode.

--- src/main/java/buildit/FooBuilder.java
+++ src/main/java/buildit/FooBuilder.java
@@ -3,10 +3,12 @@ package buildit;
 import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
+import javax.annotation.Generated;
 
 /**
  * Created by agavrilov on 24/11/14.
  */
+@Generated("")
 public class FooBuilder {
     private List<String> list;
     private int number;
@@ -59,6 +61,30 @@ public class FooBuilder {
             sb.append(", uri=").append(uri);
             sb.append('}');
             return sb.toString();
+        }
+
+        @Override
+        public boolean equals(final Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            final FooImpl foo = (FooImpl) o;
+
+            if (number != foo.number) return false;
+            if (list != null ? !list.equals(foo.list) : foo.list != null) return false;
+            if (string != null ? !string.equals(foo.string) : foo.string != null) return false;
+            if (uri != null ? !uri.equals(foo.uri) : foo.uri != null) return false;
+
+            return true;
+        }
+
+        @Override
+        public int hashCode() {
+            int result = number;
+            result = 31 * result + (string != null ? string.hashCode() : 0);
+            result = 31 * result + (uri != null ? uri.hashCode() : 0);
+            result = 31 * result + (list != null ? list.hashCode() : 0);
+            return result;
         }
     }
 
